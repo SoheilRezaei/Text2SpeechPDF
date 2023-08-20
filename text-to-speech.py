48dbc016d982c73e1b3627106b3b4f2871d02931
SoheilRezaei <soheilrezaei966+github@gmail.com>
2023-08-20 14:14:08 -0400

    Fixed a type issue

--- text-to-speech.py
+++ text-to-speech.py
@@ -1,11 +1,11 @@
-from gtts import gTTs
+from gtts import gTTS
 import os
 
 myText = 'Text to speech conversion using Python'
 
 language = 'en'
 
-output = gTTs(text=myText, lang=language, slow=False)
+output = gTTS(text=myText, lang=language, slow=False)
 
 output.save("output.mp3")
 
